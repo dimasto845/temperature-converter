9041375b6dd2f8b2ec286fe28e66a8310e422eac
Dalia <daliarojasvallejo@gmail.com>
2022-11-08 16:27:17 -0500

    Input y trimming

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    println!("Temperature transformer!");
+
+    let mut mode = String::new();
+    let mut input = String::new();
+
+    println!("What do you want to transform?\nType f for F° to C°, type c for C° to F°, type quit for exiting");
+
+    let stdin = io::stdin();
+
+    stdin.read_line(&mut mode)?;
+
+    
+    println!("Input the value");
+    stdin.read_line(&mut input)?;
+
+    let mode = mode.trim();
+    let input = input.trim();
+
+
+
+    println!("{mode} {input}");
+
+    Ok(())
 }
+
+/* fn fahrenheit_to_celsius(x: f32) -> f32 {
+    (x - 32.0) * (5.0/9.0)
+} */
+
+/* fn celsius_to_fahrenheit(x: f32) -> f32 {
+    (x * (9.0/5.0)) + 32.0
+} */
