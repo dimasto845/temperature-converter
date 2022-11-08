a7459fc835a605550c1132c0ccdfbe8aa86e6c51
Dalia <daliarojasvallejo@gmail.com>
2022-11-08 16:47:47 -0500

    Terminado

--- src/main.rs
+++ src/main.rs
@@ -22,26 +22,24 @@ fn main() -> io::Result<()> {
         } else {
             continue;
         }
-    
-        
-        
-        stdin.read_line(&mut input)?;
-    
-        let input = input.trim();
-    
-        
-    
-        println!("{mode} {input}");
 
+        stdin.read_line(&mut input)?;
+
+        let input: f32 = input.trim().parse().expect("Please input a valid number.");
+
+        let result: f32 = if mode.eq("f") {fahrenheit_to_celsius(input)} else {celsius_to_fahrenheit(input)};
+        let unit = if mode.eq("f") {"C"} else {"F"};
+
+        println!("{result} {unit}°");
     }
 
     Ok(())
 }
 
-/* fn fahrenheit_to_celsius(x: f32) -> f32 {
+fn fahrenheit_to_celsius(x: f32) -> f32 {
     (x - 32.0) * (5.0/9.0)
-} */
+}
 
-/* fn celsius_to_fahrenheit(x: f32) -> f32 {
+fn celsius_to_fahrenheit(x: f32) -> f32 {
     (x * (9.0/5.0)) + 32.0
-} */
+}
